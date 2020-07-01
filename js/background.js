bafebd59feea88c0f5799a4377e781b5b4a80248
cheetocheeto <13050178403@163.com>
2020-07-01 20:39:01 +0800

    <fix>fix bug

--- js/background.js
+++ js/background.js
@@ -3,6 +3,16 @@
 // found in the LICENSE file.
 
 'use strict';
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'www.douban.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
 
 chrome.webNavigation.onCompleted.addListener(function() {
   let blockKeyWord
